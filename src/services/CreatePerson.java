362050715ede34167c32fc6eef668ebad478e26f
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-01-02 21:29:01 -0300

    add_pontos_person

--- src/services/CreatePerson.java
+++ src/services/CreatePerson.java
@@ -20,7 +20,7 @@ public class CreatePerson {
         String nome = "Personagem Principal";
         newPerson.setName(nome);
         System.out.println("Bem vindo , você é o "+ nome);
-        newPerson.setLife(100);
+
         System.out.println("Seu personagem possui três atributos :Força, Resistencia e Velocidade");
         System.out.println("Você começa com 50 pontos em cada atributo , agora divida mais 60 pontos entre os três atributos e sua vida inicial será calculada");
         System.out.println("Pontos para Força (0-50)");
@@ -36,7 +36,39 @@ public class CreatePerson {
             forca = forca + ptsForca;
             System.out.println("Sua força é=" + forca);
         }
+        int restantes = total - ptsForca;
+        System.out.println("Pontos para velocidade"+", pontos restantes " + restantes);
+        ptsVel = scanner.nextInt();
+        if (ptsVel > restantes) {
+            ptsVel = 0;
+            System.out.println("Você não pontos suficientes, insira no maximo "+ restantes +" pontos");
+            ptsVel = scanner.nextInt();
+            vel = ptsVel + vel;
+        } else {
+            vel = ptsVel + vel;
+        }
+
+        restantes = total - ptsForca - ptsVel;
+
+        System.out.println("Pontos para resistencia"+", pontos restantes " + restantes);
+        ptsRes = scanner.nextInt();
+        if(ptsRes > restantes) {
+            ptsRes = 0;
+            System.out.println("Você não pontos suficientes, insira no maximo "+ restantes +" pontos");
+            ptsRes = scanner.nextInt();
+            res = ptsRes + res;
+        } else {
+            res = ptsRes + res;
+        }
+        int vida = (forca + vel + res)/3;
+        newPerson.setLife(vida);
         System.out.println("Sua forca é "+ forca);
+        System.out.println("Sua velocidade é "+ vel);
+        System.out.println("Sua forca é "+ res);
+        System.out.println("Sua vida é " + vida);
+
+
+
 
 
 
